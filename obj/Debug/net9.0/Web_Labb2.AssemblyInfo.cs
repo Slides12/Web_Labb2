217c0c706343094ff1f5fc1028b9ecc0266502d0
Daniel Johansson <djohan93@hotmail.com>
2025-02-23 14:25:05 +0100

    Added EFcore

--- obj/Debug/net9.0/Web_Labb2.AssemblyInfo.cs
+++ obj/Debug/net9.0/Web_Labb2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web_Labb2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a85b01c5a41dddc534695dbaec7c43674cb0eca0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9838664cc2525084d9729f907f44f50aadc56260")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web_Labb2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web_Labb2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
