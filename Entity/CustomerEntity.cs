a85b01c5a41dddc534695dbaec7c43674cb0eca0
Daniel Johansson <djohan93@hotmail.com>
2025-02-23 12:03:17 +0100

    Finished Customer Entity and address entity

---
+++ Entity/CustomerEntity.cs
@@ -0,0 +1,9 @@
+public class CustomerEntity
+{
+    public int Id { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public string? Email { get; set; }
+    public string? PhoneNumber { get; set; }
+    public AdressEntity? AddressInformation { get; set; }
+}
