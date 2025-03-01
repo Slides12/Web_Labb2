b101441bbb84be21865730cf0b59226ee9feb985
Daniel Johansson <djohan93@hotmail.com>
2025-03-01 09:18:46 +0100

    Added a Shared project to store DTO and Entitites

--- Entity/AddressEntity.cs
+++
@@ -1,9 +0,0 @@
-public class AddressEntity
-{
-    public int Id { get; set; }
-    public string? Address { get; set; }
-    public string? PostNumber { get; set; }
-    public string? City { get; set; }
-    public string? Country { get; set; }
-
-}
