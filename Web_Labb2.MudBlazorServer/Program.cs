6242ec9c1fc992c55b5ef282fb300dc55d8f54d9
Daniel Johansson <djohan93@hotmail.com>
2025-03-07 20:18:32 +0100

    Tried to add login page. Not succeeded yet.

--- Web_Labb2.MudBlazorServer/Program.cs
+++ Web_Labb2.MudBlazorServer/Program.cs
@@ -1,11 +1,19 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using MudBlazor.Services;
 using Web_Labb2.MudBlazorServer.Components;
+using Web_Labb2.MudBlazorServer.Providers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add MudBlazor services
 builder.Services.AddMudServices();
 builder.Services.AddHttpClient();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddAuthorizationCore();
+
+
+builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
